e953ee1169bb30f6cbaffa4b69e26383bbf0f21e
David <davidbrunalvarez@gmail.com>
2021-04-12 22:25:34 +0200

    same as last one

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,5 @@
 //Url de ejemplo para conseguir datos de una baliza en específico https://www.euskalmet.euskadi.eus/vamet/stations/readings/C054/2021/01/18/readingsData.json
-
+const fetch = require('node-fetch')
 const express = require('express');
 const app = express();
 const port = 20000;
@@ -11,14 +11,16 @@ const cargarListaBalizas = () => {
     //Con esta url podemos conseguir la lista de estaciónes con diferentes datos
     fetch("https://www.euskalmet.euskadi.eus/vamet/stations/stationList/stationList.json")
     .then(response => response.json())
-    .then(data => ListaBalizas.push(data));    
+    .then(data => ListaBalizas.push(data));   
 }
 
 
 
 app.get('/stationList', (req, res) => {
     cargarListaBalizas()
-    res.end(ListaBalizas);
+    let str = "";
+    ListaBalizas.forEach(baliza => {str += JSON.stringify(baliza)})
+    res.end(str);
 });
 
 app.listen(port, () => {
